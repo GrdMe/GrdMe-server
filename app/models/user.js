8a4cee16f9de8b09944b7e62e0522c2a2e2a91a2
dstantwo <dmstanley2@gmail.com>
2015-11-11 00:36:49 -0500

    Modified schema for protobufs

--- app/models/user.js
+++ app/models/user.js
@@ -4,16 +4,13 @@ var mongoose = require('mongoose');
 
 var userSchema = mongoose.Schema({
     identityKey: String, //base64 encoded pubkey
-    identityKeyBuffer: Buffer, //ArrayBuffer of identity Key used to verify sigs
     devices: [{deviceId: Number, //int
                lastResortKey: {keyId: Number, //int
-                               publicKey: String, //binary blob?
-                               identityKey: String, //base64 encoded pubkey
-                               deviceId: String}, //int
-                keys: [{keyId: String,
-                        publicKey: String,
-                        identityKey: String,
-                        deviceId: String}]
+                               key: Buffer //binary blob?
+                           },
+                prekeys: [{keyId: Number,
+                           key: Buffer
+                    }]
              }]
 });
 
