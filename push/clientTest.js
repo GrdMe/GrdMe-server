50068644c6c1f67aefb6038277f936b003190c5e
dstantwo <dmstanley2@gmail.com>
2015-12-01 19:27:01 -0500

    implementing auth for sockets

--- push/clientTest.js
+++ push/clientTest.js
@@ -1,16 +1,21 @@
 var ioClient = require('socket.io-client');
 
+// 1: vvv change url to be appropriate vvv
 var socket = ioClient.connect("http://localhost:8080");
 
 socket.on('connect', function (data) {
-    socket.emit('storeCustomPushId', { customId:"identityKey|deviceId" });
+    // 2: vvv Change value of username & password to be appropriate for specific client
+    socket.emit('authentication', { username:"identityKey|deviceId", password:"time|sig(time)" });
 });
 
 socket.on('message', function(messageData) {
-    // vvv do something with messageData here vvv
+    // 3: vvv do something with messageData here vvv
     console.log(messageData);
+    var messageHeader = messageData.header; //same header protobuff that was sent to server
+    var messageBody = messageData.body;     //same body protobuff that was sent to server
 
     // ^^^ do something with messageData here ^^^
+
     //confirm reception of message
     socket.emit('recieved', {messageId: messageData.id});
 });
--- push/socket.js
+++ push/socket.js
@@ -18,29 +18,34 @@ module.exports.listen = function(server) {
         //socket.emit('message', { hello: 'world' });
 
 
-        socket.on('storeCustomPushId', function(data) {
-            //create mapping in clients object
-            clients[data.customId] = socket.id;
-            console.log("custom Id mapped");
-            //get queued messages from DB
-            var names = data.customId.split(NAME_DELIMITER);
-            var identityKey = names[0];
-            var deviceId = names[1];
+        socket.on('authentication', function(data) {
+            if (authorize(data.username, data.password)) { //if socket authorized
+                //create mapping in clients object
+                clients[data.username] = socket.id;
+                console.log("custom Id mapped");
+                //get queued messages from DB
+                var names = data.username.split(NAME_DELIMITER);
+                var identityKey = names[0];
+                var deviceId = names[1];
 
-            MessageQueue.find({recipientIdKey : identityKey, recipientDid : deviceId}, function(err, docs){
-                if (err) console.log(err);
-                for (doc in docs) {
-                    //construct message
-                    var message = new Object();
-                    message.id = doc._id;
-                    message.header = doc.messageHeader;
-                    message.body = doc.messageBody;
-                    //push message
-                    if(clients[data.customId]) { //if client still connected
-                        socket.emit('message', message);
+                MessageQueue.find({recipientIdKey : identityKey,
+                                   recipientDid : deviceId}, function(err, docs){
+                    if (err) console.log(err);
+                    for (doc in docs) {
+                        //construct message
+                        var message = new Object();
+                        message.id = doc._id;
+                        message.header = doc.messageHeader;
+                        message.body = doc.messageBody;
+                        //push message
+                        if(clients[data.customId]) { //if client still connected
+                            socket.emit('message', message);
+                        }
                     }
-                }
-            });
+                });
+            } else { //else, socket not authorized
+
+            }
         });
 
         socket.on('recieved', function (data) {
@@ -59,3 +64,54 @@ module.exports.listen = function(server) {
         });
     });
 }
+
+var authorize = function(username, password) {
+    /* parse credentials */
+    var names = username.split(NAME_DELIMITER);
+    var identityKey = names[0];
+    var deviceId = names[1];
+    var pass = password.split(NAME_DELIMITER);
+    var authDate = pass[0];
+    var authSig = pass[1];
+
+    /* verify presence of credentials */
+    if(!username || !password || !names || names.length != 2 || !identityKey || !deviceId || !pass || pass.length != 2 || !authDate || !authSig) {
+        console.log('Socket Auth Failed: malformed credentials');
+        return false;
+    }
+
+    /* varify credentials */
+    Users.findOne({identityKey : identityKey},
+        function(err, dbUser) {
+            if ((!dbUser && !userContainsDeviceId(dbUser, deviceId) ) && !err) {
+                /* Verify date freshness */
+                var timeAuthDate = new Date(authDate);
+                var timeNow = new Date();
+                var difference = timeNow - timeAuthDate;
+                var pubkey = base64.decode(identityKey);
+                var dataToSign = base64.decode(String(authDate));
+                var signature = base64.decode(authSig);
+                var verified = crypto.verifySignature(pubkey,
+                                             dataToSign,
+                                             signature);
+                if (difference < (AUTH_CHALLENGE_TIME_TO_LIVE * 1000) && difference > 0) { //if auth is fresh
+                    /* Verify signature on date */
+                    var verified = crypto.verifySignature(pubkey, dataToSign, signature);
+                    /* return apropriate response */
+                    if (verified) { // signature on date verified
+                        return next();
+                    } else { // signature on date !verified
+                        console.log("Authentication Failed - Bad signature");
+                        return unauthorized(res, 'signature');
+                    }
+                } else { //else, auth is stale
+                    console.log("Authentication Failed - Stale date");
+                    return unauthorized(res, 'time');
+                }
+            } else { // identityKey + did combo existed in DB
+                console.log("Authentication Failed - idkey/did exist in DB");
+                return unauthorized(res, 'registered');
+            }
+        });
+
+};
